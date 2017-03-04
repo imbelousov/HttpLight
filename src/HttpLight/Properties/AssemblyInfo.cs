1c7e87431d031faf3a7445d4c691d21e1b45cb74
ibekb <bimmail@mail.ru>
2017-03-04 16:28:01 +0500

    Benchmark removed

--- src/HttpLight/Properties/AssemblyInfo.cs
+++ src/HttpLight/Properties/AssemblyInfo.cs
@@ -15,4 +15,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("HttpLight.Test")]
-[assembly: InternalsVisibleTo("HttpLight.Benchmark")]
